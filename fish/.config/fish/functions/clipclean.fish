ce09f9f70a93e29088c6e50b29c0db78f4f1ac37
Alexander Novikov <alnovis@gmail.com>
2026-02-25 15:39:19 +0300

    clipclean: rewrite to dedent clipboard instead of just trimming whitespace

--- fish/.config/fish/functions/clipclean.fish
+++ fish/.config/fish/functions/clipclean.fish
@@ -1,4 +1,26 @@
-function clipclean --description "Trim leading/trailing whitespace from clipboard"
-    pbpaste | sed 's/^[[:space:]]*//;s/[[:space:]]*$//' | pbcopy
-    echo "Clipboard cleaned"
+function clipclean --description "Dedent and trim trailing whitespace from clipboard"
+    pbpaste | awk '
+    BEGIN { min_indent = -1 }
+    {
+        lines[NR] = $0
+        if ($0 ~ /[^[:space:]]/) {
+            match($0, /^[[:space:]]*/)
+            if (min_indent == -1 || RLENGTH < min_indent) min_indent = RLENGTH
+        }
+    }
+    END {
+        if (min_indent < 1) min_indent = 0
+        for (i = 1; i <= NR; i++) {
+            line = lines[i]
+            if (length(line) > min_indent) {
+                line = substr(line, min_indent + 1)
+            } else if (line ~ /^[[:space:]]*$/) {
+                line = ""
+            }
+            sub(/[[:space:]]+$/, "", line)
+            print line
+        }
+    }
+    ' | pbcopy
+    echo "Clipboard dedented"
 end
